b397eec7e5e00952ec3e6f4a89ddfc8fb15b8196
exec zsh <marianavaladares@gmail.com>
2024-01-29 15:28:24 +0000

    added Petsitter model with migration created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_29_144420) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_29_152217) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "petsitters", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone_number"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_petsitters_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +42,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_29_144420) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "petsitters", "users"
 end
