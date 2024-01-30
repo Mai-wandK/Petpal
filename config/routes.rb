cc5df384acc5c1cdd49d945d663b10eb355e4493
Maiwand Kakar <en.maiwand@gmail.com>
2024-01-30 14:56:07 +0300

    Create routes.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  resources :users, only: [:index, :show, :edit, :update] do
+    resources :bookings, only: [:show, :new, :create, :edit, :update]
+  end
+  resources :petsitters, only: [:index, :show, :new, :create, :edit, :update] do
+    resources :bookings, only: [:show, :edit, :update]
+  end
   # Defines the root path route ("/")
   # root "posts#index"
 end
