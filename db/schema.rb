6bb4b5aaed0a88b9f8338b5eb971db035ab9d3c9
exec zsh <marianavaladares@gmail.com>
2024-01-31 13:11:39 +0000

    added changes to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2024_01_31_113241) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,11 +35,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_31_113241) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
-    t.text "photo"
-    t.float "latitude"
+    t.float "latitude"  
     t.float "longitude"
     t.string "address"
+    t.text "photo"
   end
 
   create_table "users", force: :cascade do |t|
