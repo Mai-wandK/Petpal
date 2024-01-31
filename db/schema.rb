e579da6ff5a9d75dadbd8f41797fe1c2441ea13e
Maiwand Kakar <en.maiwand@gmail.com>
2024-01-31 14:57:09 +0300

    Photo added.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_30_135216) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_31_111808) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_30_135216) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "photo"
   end
 
   create_table "users", force: :cascade do |t|
